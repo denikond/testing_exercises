837bc420d7e93d85c4fed5f7cfc4828deefb02f5
Denis Kondrashov <dkond@mail.ru>
2023-12-03 13:50:05 +0300

    Test_five_review_1

--- tests/level_1/test_five_title.py
+++ tests/level_1/test_five_title.py
@@ -9,3 +9,7 @@ def test_change_copy_item():
     assert change_copy_item("Copy of ЦБ может повысить ключевую ставку калябаля(2)", 5000) == "Copy of ЦБ может повысить ключевую ставку (3)"
     assert change_copy_item("Copy of ЦБ может повысить(2) ключевую ставку (2)калябаля", 5000) == "Copy of ЦБ может повысить(2) ключевую ставку (3)" #WTF ?
     assert change_copy_item("Copy of Copy of ЦБ может повысить ключевую ставку", 5000) == "Copy of Copy of ЦБ может повысить ключевую ставку (2)"
+    base_sting = "ЦБ может повысить"
+    for i in range(10):
+        base_sting = change_copy_item(base_sting, 5000) #return 'ЦБ может повысить'
+        assert base_sting == "Copy of ЦБ может повысить" + '' if i == 0 else ' (' + str(i) + ')'
