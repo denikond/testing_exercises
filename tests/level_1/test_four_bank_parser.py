564c411021aaf3ae1a918a2070842be067a85afd
Denis Kondrashov <dkond@mail.ru>
2023-12-03 13:54:44 +0300

    Test_four_doesnt_ready_for_review

--- tests/level_1/test_four_bank_parser.py
+++ tests/level_1/test_four_bank_parser.py
@@ -4,7 +4,20 @@ import decimal
 
 
 def test_parse_ineco_expense():
-    assert parse_ineco_expense( \
+    s_amount_sum = '1000'
+    s_amount_cents = '00'
+
+    s_last_digit = '1234'
+
+    @dataclass.dataclass()
+    class BankCardAttr:
+        last_digit: str
+        owner: str
+    ms_v_card = BankCardAttr(last_digit="3412", owner="Ms V")
+    mr_v_card = BankCardAttr(last_digit="3412", owner="Mr V")
+
+
+    assert parse_ineco_expense(
         SmsMessage("1000 00, 1234123412341234 12.11.21 15:30 300 authcode 10", "auth", datetime.datetime(2023, 1, 1)),
         [BankCard("3412", "Mr V"), BankCard("1234", "Ms V")]) == Expense(amount=decimal.Decimal('1000'),
                                                                          card=BankCard(last_digits='1234',
