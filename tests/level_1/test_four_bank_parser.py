076ec64a55802648eee3b122446113fc1826e98e
Denis Kondrashov <dkond@mail.ru>
2023-12-09 12:08:58 +0300

    test_four_review_2 quotes same, pep8 allign

--- tests/level_1/test_four_bank_parser.py
+++ tests/level_1/test_four_bank_parser.py
@@ -3,23 +3,24 @@ import datetime
 import decimal
 import dataclasses
 
+
 def test_parse_ineco_expense():
     sms_amount_sum = '1000.89'
     sms_something1 = 'something'
     sms_card_number = '1234123412341234'
-    sms_spent_at_strict_format = "12.11.21 15:30"
-    sms_spent_in: str = "something long string"
-    sms_authcode_strict_format_separator = "authcode"
-    sms_authcode_nonstrict = "something"
+    sms_spent_at_strict_format = '12.11.21 15:30'
+    sms_spent_in: str = 'something long string'
+    sms_authcode_strict_format_separator = 'authcode'
+    sms_authcode_nonstrict = 'something'
     spent_at_datetime_format = datetime.datetime.strptime(sms_spent_at_strict_format, '%d.%m.%y %H:%M')
-    ms_v_card = BankCard(last_digits="1234", owner="Ms V")
-    mr_v_card = BankCard(last_digits="3412", owner="Mr V")
+    ms_v_card = BankCard(last_digits='1234', owner='Ms V')
+    mr_v_card = BankCard(last_digits='3412', owner='Mr V')
     cards = [ms_v_card, mr_v_card]
-    author = "John Smith"
+    author = 'John Smith'
 
-    sms: str = "{0} {1}, {2} {3} {4} {5} {6}".format(sms_amount_sum, sms_something1, sms_card_number,
-                                                sms_spent_at_strict_format, sms_spent_in,
-                                                sms_authcode_strict_format_separator, sms_authcode_nonstrict)
+    sms: str = '{0} {1}, {2} {3} {4} {5} {6}'.format(sms_amount_sum, sms_something1, sms_card_number,
+                                                     sms_spent_at_strict_format, sms_spent_in,
+                                                     sms_authcode_strict_format_separator, sms_authcode_nonstrict)
 
     assert (parse_ineco_expense(
         SmsMessage(sms, author, datetime.datetime(2023, 1, 1)), cards) ==
