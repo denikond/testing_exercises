6189984aa1418b65a7edb661ee08f1c4236582b8
Denis Kondrashov <dkond@mail.ru>
2023-12-09 12:00:23 +0300

    test_three_review_2

--- tests/level_1/test_three_url_builder.py
+++ tests/level_1/test_three_url_builder.py
@@ -2,5 +2,6 @@ from functions.level_1.three_url_builder import build_url
 
 
 def test_build_url():
-    assert build_url("http://localhost", "main", dict(short='dict', long='dictionary')) == 'http://localhost/main?short=dict&long=dictionary'
+    assert (build_url("http://localhost", "main", {'short': 'dict', 'long': 'dictionary'}) ==
+            'http://localhost/main?short=dict&long=dictionary')
     assert build_url("http://localhost", "main") == 'http://localhost/main'
