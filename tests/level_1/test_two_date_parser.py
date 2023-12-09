c7ee3679755fa6607dfed835d3389f29a18e6843
Denis Kondrashov <dkond@mail.ru>
2023-12-09 12:05:10 +0300

    test_two_review_2

--- tests/level_1/test_two_date_parser.py
+++ tests/level_1/test_two_date_parser.py
@@ -2,17 +2,24 @@ from unittest import mock
 import datetime
 from functions.level_1.two_date_parser import compose_datetime_from
 
+
 @mock.patch('functions.level_1.two_date_parser.datetime.date')
 def test_compose_datetime_from(mock_date):
     mock_date.today.return_value = datetime.datetime(2012, 1, 1)
 
-    assert compose_datetime_from("", time_str="18:12") == datetime.datetime(2012, 1, 1, 18, 12)
-    assert compose_datetime_from("tomorrow", time_str="18:12") == datetime.datetime(2012, 1, 2, 18, 12)
+    assert (compose_datetime_from("", time_str="18:12") ==
+            datetime.datetime(2012, 1, 1, 18, 12))
+    assert (compose_datetime_from("tomorrow", time_str="18:12") ==
+            datetime.datetime(2012, 1, 2, 18, 12))
 
     mock_date.today.return_value = datetime.datetime(1912, 1, 1)
-    assert compose_datetime_from("", time_str="18:12") == datetime.datetime(1912, 1, 1, 18, 12)
-    assert compose_datetime_from("tomorrow", time_str="18:12") == datetime.datetime(1912, 1, 2, 18, 12)
+    assert (compose_datetime_from("", time_str="18:12") ==
+            datetime.datetime(1912, 1, 1, 18, 12))
+    assert (compose_datetime_from("tomorrow", time_str="18:12") ==
+            datetime.datetime(1912, 1, 2, 18, 12))
 
     mock_date.today.return_value = datetime.datetime(3912, 1, 1)
-    assert compose_datetime_from("", time_str="18:12") == datetime.datetime(3912, 1, 1, 18, 12)
-    assert compose_datetime_from("tomorrow", time_str="18:12") == datetime.datetime(3912, 1, 2, 18, 12)
+    assert (compose_datetime_from("", time_str="18:12") ==
+            datetime.datetime(3912, 1, 1, 18, 12))
+    assert (compose_datetime_from("tomorrow", time_str="18:12") ==
+            datetime.datetime(3912, 1, 2, 18, 12))
