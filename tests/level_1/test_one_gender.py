1edcfbbabdd930ca9c012e8471bfde3e478783bf
Denis Kondrashov <dkond@mail.ru>
2023-12-09 11:58:20 +0300

    test_one_review_2

--- tests/level_1/test_one_gender.py
+++ tests/level_1/test_one_gender.py
@@ -2,6 +2,6 @@ from functions.level_1.one_gender import genderalize
 
 
 def test_genderalize():
-    assert genderalize("Петров","Петрова","male") == "Петров"
-    assert genderalize("Петров","Петрова","female") == "Петрова"
+    assert genderalize("Петров", "Петрова", "male") == "Петров"
+    assert genderalize("Петров", "Петрова", "female") == "Петрова"
     assert genderalize("Петров", "Петрова", "male") != "Петрова"
